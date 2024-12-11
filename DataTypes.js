b61a9c4cd3132462624f4f2c5901df3ded01f4a4
Preet <preet_231@icloud.com>
2024-12-11 16:28:25 +1100

    Javascript Data Types

---
+++ DataTypes.js
@@ -0,0 +1,12 @@
+// Data types in JS
+let num = 2; //Integer 
+let num1 = 1.2 // floating point
+let str = "hello world" // string
+let IsCoding = true; //yes
+let IsOld = false; //no
+let age = null;
+
+let person = {
+    name: "Preet",
+    age : 21
+};
