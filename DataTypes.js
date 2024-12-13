637db2ffacda8e02ac031d805a0adf1104648e1c
Preet <preet_231@icloud.com>
2024-12-14 10:26:27 +1100

    Learning Operators

--- DataTypes.js
+++ DataTypes.js
@@ -6,43 +6,7 @@ let IsCoding = true; //yes
 let IsOld = false; //no
 let age = null;
 
-
-let name = "Preet";
-let age1 = 21;
-
-console.log(`My name is ${name} and I am ${age1} year
-s old!.`);
-
-//Calculating Areas
-const length = 10;
-const width = 10;
-let rectangleArea = length * width;
-
-const radius = 7;
-const pi = 3.14159;
-let circleArea = pi * radius ** 2;
-
-
-
-console.log(`Rectangle Area: ${rectangleArea}`);
-console.log(`Cricle Area: ${circleArea}`);
-
-let username = "Preet";
-let isLoggedIn = true;
-let score = 100;
-
-console.log(typeof username);
-console.log(typeof isLoggedIn);
-console.log(typeof score);
-
-// Calculate Cost 
-let income = 2500;
-let rent = 500;
-let food = 200;
-let cost = income - rent - food;
-
-console.log(`Income: $${income}`);
-console.log(`Expense: $${rent}`);
-console.log(`Food: $${food}`);
-console.log(`Saving: $${cost}`);
-
+let person = {
+    name: "Preet",
+    age : 21
+};
---
+++ Operator.js
@@ -0,0 +1,69 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+console.log("");
+let x = 5;
+let y = "5";
+
+console.log(x == y);
+console.log(x === y);
+console.log(x !== y);
+console.log(x > 3);
+console.log(x <= 5);
+
+console.log("");
+let age = 25;
+console.log(age > 18 && age < 30);
+console.log(age > 30 || age < 20);
+console.log(!(age > 18));
+
+console.log("");
+
+let num1 = 25;
+let num2 = 4;
+
+console.log("Sum:", num1 + num2);
+console.log("Remainder:", num1 + num2);
+
+console.log("");
+
+let number = 50;
+
+if(number >= 1 && number <= 100){
+    console.log("The number is within the range.");
+} else {
+    console.log("The number is out of range.");
+}
+
+let password = "secure123";
+if(password.length >= 8 && password.includes("123")) {
+    console.log("Password is strong!");
+} else {
+    console.log("Password is weak!");
+}
+
+console.log("");
+
+let number1 = 7;
+if (number % 2 === 0) {
+    console.log(`${number1} is even`);
+} else {
+    console.log(`${number1} is odd`);
+}
+
+let score = 75;
+
+if (score >= 90) {
+    console.log("Grade: A");
+} else if(score >= 80) {
+    console.log("Grade: B");
+} else if(score >= 70) {
+    console.log("Grade: C");
+} 
